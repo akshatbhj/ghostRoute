56304b24152ff5c2f2bbbef8e26ce815f6296b98
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2026-07-02 13:34:54 +0530

    Set up database connection. Resolves #2

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from backend!");
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Successfully connected to MongoDB Vault!"))
+  .catch((err) => console.error("❌ Database connection error:", err));
+
+app.get("/test", (req, res) => {
+  res.json({message: "✅GhostRoute server is successfully running..."});
 });
 
 app.listen(port, () => {
-  console.log(`server is listening on port : ${port}`);
+  console.log(`🚀 Server is listening on port : ${port}`);
 });
